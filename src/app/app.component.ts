0eb7a6079e413b5b02a644d9081c0cf7b3ec19fd
Vikash Kumar Bijarnia <vkb.info@gmail.com>
2018-11-05 23:03:32 +0530

    implemented ball game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-gameball';
+  score = 0;
+  ballCount = 0;
+  ballArray = [];
+//   If there are no balls in the box it inserts a pink ball.
+//   Every third ball is a green ball.
+//   Every fifth ball is a blue ball.
+//   Every fifteenth ball is a purple ball.
+//   Every other ball is a pink ball.
+
+// The sum shows the number of balls.
+
+// The score shows the sum of the balls scores:
+
+//   Pink Ball: 1
+//   Green Ball: 3
+//   Blue Ball: 5
+//   Purple Ball: 15
+  addABall() {
+    this.ballCount += 1;
+    let ball = 'pink';
+    if (this.ballCount === 1) {
+      this.score += 1;
+    } else if ((this.ballCount % 15) === 0) {
+      ball = 'purple';
+      this.score += 15;
+    } else if ((this.ballCount % 3) === 0) {
+      ball = 'green';
+      this.score += 3;
+    } else if ((this.ballCount % 5) === 0) {
+      ball = 'blue';
+      this.score += 5;
+    } else {
+      this.score += 1;
+      ball = 'pink';
+    }
+    this.ballArray.push(ball);
+    console.log(this.ballArray);
+  }
 }
